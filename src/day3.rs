dc8f007cd9ba2104723b481546c2bcc8c3d16119
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2023-12-04 12:43:01 -0500

    day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use aoc_runner_derive::{aoc, aoc_generator};
 use regex::Regex;
 
@@ -35,14 +37,21 @@ const NEIGHBORS: [(i32, i32); 8] = [
     (1, 1),
 ];
 
+const NEIGHBORS_RIGHT: [(i32, i32); 3] = [
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
 #[aoc(day3, part1)]
 pub fn solve_part1(input: &(Vec<Part>, Vec<Vec<char>>)) -> u32 {
     let (parts, schematic) = input;
     let mut sum = 0u32;
     for part in parts {
         let mut coords = part.coords;
-        'partloop: for _ in 0..part.length {
-            for neighbor in NEIGHBORS {
+        'partloop: for i in 0..part.length {
+            let neighbors = if i == 0 { NEIGHBORS.iter() } else { NEIGHBORS_RIGHT.iter() };
+            for neighbor in neighbors {
                 let copy = (coords.0 + neighbor.0, coords.1 + neighbor.1);
                 let line = schematic.get(copy.1 as usize);
                 if line.is_none() {
@@ -61,6 +70,41 @@ pub fn solve_part1(input: &(Vec<Part>, Vec<Vec<char>>)) -> u32 {
                 }
             }
             coords.0 += 1;
+        }
+    }
+    sum
+}
+
+#[aoc(day3, part2)]
+pub fn solve_part2(input: &(Vec<Part>, Vec<Vec<char>>)) -> u32 {
+    let (parts, schematic) = input;
+    let mut gears = HashMap::new();
+    let mut sum = 0u32;
+    for part in parts {
+        let mut coords = part.coords;
+        for i in 0..part.length {
+            let neighbors = if i == 0 { NEIGHBORS.iter() } else { NEIGHBORS_RIGHT.iter() };
+            for neighbor in neighbors {
+                let copy = (coords.0 + neighbor.0, coords.1 + neighbor.1);
+                let line = schematic.get(copy.1 as usize);
+                if line.is_none() {
+                    continue;
+                }
+                let symbol = line.unwrap().get(copy.0 as usize);
+                if symbol.is_none() {
+                    continue;
+                }
+                if *symbol.unwrap() == '*' {
+                    let gear = gears.entry(copy).or_insert(Vec::new());
+                    gear.push(part.value);
+                }
+            }
+            coords.0 += 1;
+        }
+    }
+    for (_, gear) in gears {
+        if gear.len() == 2 {
+            sum += gear[0] * gear[1];
         }
     }
     sum
