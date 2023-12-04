49883843c0b9574e6b1426686cb50dc68cd2fb91
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2023-12-04 13:50:59 -0500

    day 4 part 1

--- src/day4.rs
+++ src/day4.rs
@@ -2,6 +2,7 @@ use aoc_runner_derive::{aoc, aoc_generator};
 use regex::Regex;
 
 pub struct Card {
+    pub count: u32,
     winning: Vec<u32>,
     numbers: Vec<u32>,
 }
@@ -11,14 +12,17 @@ pub fn input_generator(input: &str) -> Vec<Card> {
     let mut cards = Vec::new();
     let re = Regex::new(r"\d+").unwrap();
     for line in input.lines() {
-        let numbers = input.split(':').collect::<Vec<_>>()[1];
+        let numbers = line.split(':').collect::<Vec<_>>()[1];
         let parts = numbers.split('|').collect::<Vec<_>>();
-        let winning = re.find_iter(parts[0]).map(|c| c.as_str().parse::<u32>().unwrap()).collect::<Vec<_>>();
-        let numbers = re.find_iter(parts[1]).map(|c| c.as_str().parse::<u32>().unwrap()).collect::<Vec<_>>();
-        cards.push(Card {
-            winning,
-            numbers,
-        });
+        let winning = re
+            .find_iter(parts[0])
+            .map(|c| c.as_str().parse::<u32>().unwrap())
+            .collect::<Vec<_>>();
+        let numbers = re
+            .find_iter(parts[1])
+            .map(|c| c.as_str().parse::<u32>().unwrap())
+            .collect::<Vec<_>>();
+        cards.push(Card { count: 1, winning, numbers });
     }
     cards
 }
@@ -26,6 +30,15 @@ pub fn input_generator(input: &str) -> Vec<Card> {
 #[aoc(day4, part1)]
 pub fn solve_part1(input: &Vec<Card>) -> u32 {
     let mut sum = 0u32;
-    todo!();
+    for card in input {
+        let count = card
+            .numbers
+            .iter()
+            .filter(|x| card.winning.contains(x))
+            .count() as u32;
+        if count > 0 {
+            sum += 2u32.pow(count - 1);
+        }
+    }
     sum
 }
