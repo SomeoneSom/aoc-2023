11268f4a63250d295b792b63a71abf9e6460ae3d
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2023-12-04 11:04:03 -0500

    day 1 part 1

--- src/day1.rs
+++ src/day1.rs
@@ -2,5 +2,12 @@ use aoc_runner_derive::aoc;
 
 #[aoc(day1, part1)]
 pub fn solve_part1(input: &str) -> u32 {
-    todo!();
+    let mut sum = 0u32;
+    for line in input.split('\n') {
+        let chars = line.chars();
+        let digits = chars.filter_map(|c| c.to_digit(10)).collect::<Vec<_>>();
+        sum += digits[0] * 10;
+        sum += digits.last().unwrap();
+    }
+    sum
 }
