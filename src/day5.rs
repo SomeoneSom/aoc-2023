9ec212949c56be2b69da0e3afb9c67cbf243d839
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2023-12-05 08:18:33 -0500

    day 5 part 1

--- src/day5.rs
+++ src/day5.rs
@@ -1,12 +1,42 @@
 use aoc_runner_derive::{aoc_generator, aoc};
 use regex::Regex;
 
+pub struct Input {
+    seeds: Vec<i64>,
+    maps: Vec<Vec<(i64, i64, i64)>>,
+}
+
 #[aoc_generator(day5)]
-pub fn input_generator(input: &str) -> _ {
-    todo!();
+pub fn input_generator(input: &str) -> Input {
+    let re = Regex::new(r"\d+").unwrap();
+    let parts = input.split(':').collect::<Vec<_>>();
+    let seeds = re.find_iter(parts[1]).map(|c| c.as_str().parse::<i64>().unwrap()).collect();
+    let mut maps = Vec::new();
+    for part in parts[2..].iter() {
+        let matches = re.find_iter(part).map(|c| c.as_str().parse::<i64>().unwrap()).collect::<Vec<_>>();
+        maps.push(matches.chunks(3).map(|m| 
+            (m[1], m[1] + m[2] - 1, m[0] - m[1]) // start, end, offset
+        ).collect());
+    }
+    Input {
+        seeds,
+        maps,
+    }
 }
 
 #[aoc(day5, part1)]
-pub fn solve_part1(input: &_) -> u32 {
-    todo!();
+pub fn solve_part1(input: &Input) -> i64 {
+    let locations = input.seeds.iter().map(|s| {
+        let mut seed = *s;
+        for map in &input.maps {
+            for range in map {
+                if seed >= range.0 && seed <= range.1 {
+                    seed += range.2;
+                    break;
+                }
+            }
+        }
+        seed
+    });
+    locations.min().unwrap()
 }
