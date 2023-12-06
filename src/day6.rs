ad4c5b7f28f022dbae296891eb4b488d70277172
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2023-12-06 07:48:01 -0500

    day 6 setup

---
+++ src/day6.rs
@@ -0,0 +1,12 @@
+use aoc_runner_derive::{aoc_generator, aoc};
+use regex::Regex;
+
+#[aoc_generator(day6)]
+pub fn input_generator(input: &str) -> _ {
+    todo!();
+}
+
+#[aoc(day6, part1)]
+pub fn solve_part1(input: &_) -> _ {
+    todo!();
+}
